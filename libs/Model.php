077b0a2967a3343d1091093a4328782cd094e41c
Krishalika <krishalikadilanirathnayake@gmail.com>
2020-06-25 10:38:14 +0530

    updated database connection with Singleto pattern

--- libs/Model.php
+++ libs/Model.php
@@ -1,12 +1,70 @@
 <?php
-class Model
-{
-    function __construct()
-    {
+// class Model
+// {
+//     function __construct()
+//     {
      
-        $pdo = new PDO('mysql:host=localhost; port=8080; dbname=moh','root','');
-        $pdo-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//         $pdo = new PDO('mysql:host=localhost; port=8080; dbname=moh','root','');
+//         $pdo-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+//     }
+// }
+?> 
+
+<?php
+
+class Model{
+    private $host;
+    private $user;
+    private $password;
+    private $dbName;
+    private $connection;
+    private $results;
+
+    private static $dbInstance;
+
+    private function __construct()
+    {
+    }
+
+    public static function getInstance()
+    {
+        if (! self::$dbInstance){
+            self::$dbInstance = new self();
+        }
+        return self::$dbInstance;
+    }
+
+    function connect($host, $user, $password, $dbName){
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+        $this->dbName = $dbName;
+        $this->connection = mysqli_connect($this->host,$this->user,$this->password,$this->dbName);
+    }
+
+    public function closeConnection(){
+        mysqli_close($this->connection);
+    }
+
+    public function doQuery($sql){
+        $this->results = mysqli_query($this->connection, $sql) or die("");
+    }
+
+    public function loadObjList(){
+        $objArray = null;
+        if($this->results){
+            $objArray = mysqli_fetch_assoc($this->results);
+        }
+        return $objArray;
+    }
+
+    public function getConnection(){
+        return $this->connection;
+    }
+    public function getResult(){
+        return $this->results;
     }
 }
+
 ?>
