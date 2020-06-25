ae3da4c84e73089516e5fafe3b50d800ae204f3f
Krishalika <krishalikadilanirathnayake@gmail.com>
2020-06-25 10:41:08 +0530

    updated database connection with Singleton pattern

--- libs/Model.php
+++ libs/Model.php
@@ -1,17 +1,4 @@
 <?php
-// class Model
-// {
-//     function __construct()
-//     {
-     
-//         $pdo = new PDO('mysql:host=localhost; port=8080; dbname=moh','root','');
-//         $pdo-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-//     }
-// }
-?> 
-
-<?php
 
 class Model{
     private $host;
