6db30f676b9f691f9a988de5fafc41c1bf4d281e
Jamini <jamini.18@cse.mrt.ac.lk>
2020-06-27 18:44:19 +0530

    prototype design pattern done

--- application/controllers/admin/PatientChild.php
+++ application/controllers/admin/PatientChild.php
@@ -1,13 +1,23 @@
 <?php
 require_once "User_1.php";
+
+//--------------------class which is used prototype--------------------
 class PatientChild extends User_1{
-    function _construct($username, $password, $category){
-        parent::construct($username, $password, $category);
+    public function _construct($username, $password, $category){
+        $prototype=new PatientMother($username, $password, $category);
+        $baby=clone $prototype;//-----------create the clone of PatientMother as $baby
+        $this->username = $baby->get_username();
+        $this->password = $baby->get_password();
+        $this->category = $baby->get_category();
+        //return ($baby);
     }
 }
+// $baby=new PatientChild('niwi','12','patient');
+// var_dump($baby);
 
-//  $john=new Doctor('John','23','CEO','15000');
-// echo $john->get_id();
-// echo $john->get_username();
-// echo $john->get_category();
+
+// $baby_1=new PatientChild('niwithi','2','patient');
+// var_dump($baby_1);
+// echo $baby_1->get_username();
+
 ?>
