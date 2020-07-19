f5325faa95fc25daf0cb5f36118d72ccc2fdf900
Krishalika <krishalikadilanirathnayake@gmail.com>
2020-07-19 18:08:18 +0530

    changes and added full database

---
+++ application/views/contents_page.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <title>Contents</title>
+    <link rel = "stylesheet" href ="../../public/css/BasicDetails.css">
+</head>
+
+<header>
+<div class="header"><img class="logo" src="../../public/images/logo.jpg"/>
+<div class="logo">
+		<h1>Medical Officer of Health Office</h1>
+		<h3>Gampaha</h3>
+	</div>
+    <div class="topnav">
+  
+  <a href="login_page.php">Log out</a>
+  <a href="home.php">Home</a>
+  
+  
+</div>
+	</div>
+</header>
+</html>
