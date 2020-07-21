cd63bfb404778d056ad2a1a27ec23f60f3c6051a
Supuni-Jayasinghe <63720402+Supuni-Jayasinghe@users.noreply.github.com>
2020-07-21 21:10:22 +0530

    completed content page

--- application/views/contents_page.php
+++ application/views/contents_page.php
@@ -1,22 +1,61 @@
+<!DOCTYPE html>
 <html>
-<head>
-    <title>Contents</title>
-    <link rel = "stylesheet" href ="../../public/css/BasicDetails.css">
-</head>
-
-<header>
-<div class="header"><img class="logo" src="../../public/images/logo.jpg"/>
-<div class="logo">
-		<h1>Medical Officer of Health Office</h1>
-		<h3>Gampaha</h3>
-	</div>
-    <div class="topnav">
-  
-  <a href="login_page.php">Log out</a>
-  <a href="home.php">Home</a>
-  
-  
-</div>
-	</div>
-</header>
+    <head>
+        <title>Content</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel = "stylesheet" href ="../../public/css/icon.css">
+    </head>
+    <head>
+        <body>
+            <div class="hero-image">
+                <img src="../../public/images/logo.png" class="logo-image"/>
+                <img src="../../public/images/line.jpg" class="line-image"/>
+                <a href="login_page.php"><img src="../../public/images/logout.png" class="logout-image"></a>
+                <a href="home.php"><img src="../../public/images/home.png" class="home-image"></a>
+                <div class="hero-text">
+                    <h1 style="font-size:25px">Medical Officer of Health Office</h1>
+                    <h3 style="font-size:25px">Gampaha</h3>
+                </div>
+            </div>
+            <fieldset>
+                <legend height="100%;"><img src="../../public/images/content.png" class="content-image"/></legend>
+                <fieldset>
+                    <legend height="80%;" ><img src="../../public/images/mom.gif" class="mom-image"/></legend>
+                    <a href="BasicDetails_page.php"><img src="../../public/images/ac1.gif" class="button-image"></a>
+                    <a href="personalInfo_page.php"><img src="../../public/images/ac2.gif" class="button-image"></a>
+                    <a href="clinic_day.php"><img src="../../public/images/ac3.gif" class="button-image"></a>
+                    <a href="investigations_page.php"><img src="../../public/images/ac4.gif" class="button-image"></a>
+                    <a href="WeightGainChart.html"><img src="../../public/images/ac5.gif" class="button-image"></a>
+                    <a href="IECMaterial_page.php"><img src="../../public/images/ac6.gif" class="button-image"></a>
+                    <a href="HosClinicCare_page.php"><img src="../../public/images/ac7.gif" class="button-image"></a>
+                    <a href="USScan_page.php"><img src="../../public/images/ac8.gif" class="button-image"></a>
+                    <a href="DelivPostnatalCare_page.php"><img src="../../public/images/ac9.gif" class="button-image"></a>
+                    <a href="postPartumFC_page.php"><img src="../../public/images/ac10.gif" class="button-image"></a>
+                </fieldset>
+                <fieldset>
+                <legend height="80%;"><img src="../../public/images/child.gif" class="child-image" /></legend>
+                <a href="childDetails_page.php"><img src="../../public/images/pc1.gif" class="button-image"></a>
+                <a href="childDetails2_page.php"><img src="../../public/images/pc2.gif" class="button-image"></a>
+                <a href="immunization_page.php"><img src="../../public/images/pc3.gif" class="button-image"></a>
+                <a href="Refer1_page.php"><img src="../../public/images/pc4.gif" class="button-image"></a>
+                <a href="Refer2_page.php"><img src="../../public/images/pc5.gif" class="button-image"></a>
+                <a href="childVision_page.php"><img src="../../public/images/pc6.gif" class="button-image"></a>
+                <a href="childHearing_page.php"><img src="../../public/images/pc7.gif" class="button-image"></a>
+                <a href="Birthto18_page.php"><img src="../../public/images/pc8.gif" class="button-image"></a>
+                <a href="M18_5year_page.php"><img src="../../public/images/pc9.gif" class="button-image"></a>
+                <a href="ChildHealth1_page.php"><img src="../../public/images/pc10.gif" class="button-image"></a>
+                <a href="ChildHealth2_page.php"><img src="../../public/images/pc11.gif" class="button-image"></a>
+                <a href="Nursery1_page.php"><img src="../../public/images/pc12.gif" class="button-image"></a>
+                <a href="Nursery2_page.php"><img src="../../public/images/pc13.gif" class="button-image"></a>
+            </fieldset>
+                
+            </fieldset>
+            
+            <footer>
+                <p style="text-align: center;">Copyright @ 2020 MOH, Gampaha, Sri Lanka</br>tele: 033-2222278</br>fax: 033-2222278</br>email: gampahamoh@yahoo.com </p>
+            </footer>
+           
+            
+        </body>
+    </head>
 </html>
