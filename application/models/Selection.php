dc3ff811def77ca594740b78241545552f299eaa
Supuni-Jayasinghe <63720402+Supuni-Jayasinghe@users.noreply.github.com>
2020-08-06 19:04:00 +0530

    Update Selection.php

--- application/models/Selection.php
+++ application/models/Selection.php
@@ -2,11 +2,11 @@
 Class Selection{
     function myFunction($id){
         if(strlen($id)==10) {
-            echo("<p style='font-size: 25px;border:1.5px solid black; height: 5%; width:50%;transform: translate(380px,650px); text-align: center;background-color:rgb(8, 184, 125);'>"."<a href='Client_pdf.php' style='color:white;'>Mother's Report</a>"."</p>");
+            echo("<p style='font-size: 17px;border:1px solid black; position:absolute; height: 30px; width:25%;transform: translate(147%,500px); border-radius:5%; text-align: center;background-color:rgb(8, 184, 125);'>"."<a href='Client_pdf.php' style='color:black;'>Mother's Report</a>"."</p>");
         
         }
         else{
-            echo("<p style='font-size: 25px;border:1.5px solid black; height: 5%; width:50%;transform: translate(380px,650px); text-align: center;background-color:rgb(8, 184, 125);'>"."<a href='Client.php' style='color:white;'>Child's Report</a>"."</p>");
+            echo("<p style='font-size: 17px;border:1px solid black; position:absolute; height: 30px; width:25%;transform: translate(147%,500px); border-radius:5%; text-align: center;background-color:rgb(8, 184, 125);'>"."<a href='Client.php' style='color:black;'>Child's Report</a>"."</p>");
         }}
 }
 
