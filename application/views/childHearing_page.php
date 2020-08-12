ba427e9d37cfdde72158d8785ec04ad51ea9b556
Adeesha <63795833+anw98@users.noreply.github.com>
2020-08-12 11:45:54 +0530

    Update childHearing_page.php

--- application/views/childHearing_page.php
+++ application/views/childHearing_page.php
@@ -6,7 +6,7 @@ require '../models/childHearing_model.php';
 <head></head>
 <body>
 <form method="POST" action="childHearing_page.php">
-<input type="text" name="patient_id" placeholder="රෝගියාගේ හැඳුනුම්පත් අංකය සඳහන් කරන්න/Enter patient's NIC here" value="<?php echo($patient_id);?>"><br><br>
+<input type="text" name="id" placeholder="රෝගියාගේ හැඳුනුම්පත් අංකය සඳහන් කරන්න/Enter patient's NIC here" value="<?php echo($id);?>"><br><br>
 
 <input type="submit" name="Search" value="Search">
 <input type="submit" name="Update" value="Update"><br><br>
