15ca4c3813119337f56ce45bcedcc700fc7213d3
Adeesha <63795833+anw98@users.noreply.github.com>
2020-08-12 11:13:09 +0530

    Update d_HosClinicCare.php

--- Our forms/d_HosClinicCare.php
+++ Our forms/d_HosClinicCare.php
@@ -170,7 +170,7 @@ td, th {
 <body>
 
 <form method="POST" action="page5.php">
-<input type="text" name="id" placeholder="රෝගියාගේ හැඳුනුම්පත් අංකය සඳහන් කරන්න/Enter patient's NIC here" value="<?php echo($patient_id);?>"><br><br>
+<input type="text" name="id" placeholder="රෝගියාගේ හැඳුනුම්පත් අංකය සඳහන් කරන්න/Enter patient's NIC here" value="<?php echo($id);?>"><br><br>
 
 <input type="submit" name="Search" value="Search">
 <input type="submit" name="Update" value="Update"><br><br>
