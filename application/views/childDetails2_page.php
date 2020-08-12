08c017601a03bc75d2d96781f120d4c3a8e2ae22
Adeesha <63795833+anw98@users.noreply.github.com>
2020-08-12 11:43:44 +0530

    Update childDetails2_page.php

--- application/views/childDetails2_page.php
+++ application/views/childDetails2_page.php
@@ -11,7 +11,7 @@ require '../models/childDetails2_model.php';
 <body>
 
 <form method="POST" action="childDetails2_page.php">
-<input type="text" name="patient_id" placeholder="රෝගියාගේ හැඳුනුම්පත් අංකය සඳහන් කරන්න/Enter patient's NIC here" value="<?php echo($patient_id);?>"><br><br>
+<input type="text" name="id" placeholder="රෝගියාගේ හැඳුනුම්පත් අංකය සඳහන් කරන්න/Enter patient's NIC here" value="<?php echo($id);?>"><br><br>
 
 <input type="submit" name="Search" value="Search">
 <input type="submit" name="Update" value="Update"><br><br>
