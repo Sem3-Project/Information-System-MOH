26e50a90315dc62bd4effbe5cf0a4f069199e7f1
Adeesha <63795833+anw98@users.noreply.github.com>
2020-08-12 11:17:00 +0530

    Update d_childDetails2.php

--- Our forms/d_childDetails2.php
+++ Our forms/d_childDetails2.php
@@ -4,7 +4,7 @@ $user = "root";
 $password ="123456";
 $database="moh";
 
-$patient_id='';
+$id='';
 $dat1='';
 $dat2='';
 $dat3='';
@@ -65,7 +65,7 @@ try{
 //get data from the form
 function getData(){
     $data=array();
-    $data[0]=(isset($_POST['patient_id']) ? $_POST['patient_id'] : '');
+    $data[0]=(isset($_POST['id']) ? $_POST['id'] : '');
     $data[1]=(isset($_POST['dat1']) ? $_POST['dat1'] : '');
     $data[2]=(isset($_POST['dat2']) ? $_POST['dat2'] : '');
     $data[3]=(isset($_POST['dat3']) ? $_POST['dat3'] : '');
@@ -122,7 +122,7 @@ function getData(){
   //search
   if(isset($_POST['Search'])){
       $info=getData();
-      $search_query="SELECT * FROM childdata2 WHERE patient_id='$info[0]'";
+      $search_query="SELECT * FROM childdata2 WHERE id='$info[0]'";
       $search_result=mysqli_query($connect,$search_query);
           if($search_result){
               if($search_result){
@@ -190,7 +190,7 @@ function getData(){
       //insert
       if(isset($_POST['Insert'])){
           $info=getData();
-          $insert_query="INSERT INTO `childdata2`(`patient_id`, `dat1`, `dat2`, `dat3`, `dat4`, `scolor1`, `scolor2`, `scolor3`, `scolor4`, `eye1`, `eye2`, `eye3`, `eye4`, 
+          $insert_query="INSERT INTO `childdata2`(`id`, `dat1`, `dat2`, `dat3`, `dat4`, `scolor1`, `scolor2`, `scolor3`, `scolor4`, `eye1`, `eye2`, `eye3`, `eye4`, 
           `sc1`, `sc2`, `sc3`, `sc4`, `m1`, `m2`, `m3`, `m4`, `rel1`, `rel2`, `rel3`, `rel4`, `other1`, `other2`, `other3`, `other4`, `date1`, `date2`, `date3`, `date4`, 
           `date5`, `date6`, `date7`, `date8`, `date9`, `date10`, `date11`, `date12`, `date13`, `date14`, `date15`, `date16`, `date17`, `date18`, `date19`, `date20`) 
           VALUES ('$info[0]','$info[1]','$info[2]','$info[3]','$info[4]','$info[5]','$info[6]','$info[7]','$info[8]','$info[9]','$info[10]','$info[11]','$info[12]'
@@ -215,7 +215,7 @@ function getData(){
     //update
     if(isset($_POST['Update'])){
         $info=getData();
-        $update_query="UPDATE `childdata2` SET `patient_id`='$info[0]',`dat1`='$info[1]',`dat2`='$info[2]',`dat3`='$info[3]',`dat4`='$info[4]',`scolor1`='$info[5]',`scolor2`='$info[6]',`scolor3`='$info[7]',
+        $update_query="UPDATE `childdata2` SET `id`='$info[0]',`dat1`='$info[1]',`dat2`='$info[2]',`dat3`='$info[3]',`dat4`='$info[4]',`scolor1`='$info[5]',`scolor2`='$info[6]',`scolor3`='$info[7]',
         `scolor4`='$info[8]',`eye1`='$info[9]',`eye2`='$info[10]',`eye3`='$info[11]',`eye4`='$info[12]',`sc1`='$info[13]',`sc2`='$info[14]',`sc3`='$info[15]',`sc4`='$info[16]',`m1`='$info[17]',`m2`='$info[18]',
         `m3`='$info[19]',`m4`='$info[20]',`rel1`='$info[21]',`rel2`='$info[22]',`rel3`='$info[23]',`rel4`='$info[24]',`other1`='$info[25]',`other2`='$info[26]',`other3`='$info[27]',`other4`='$info[28]',`date1`='$info[29]',
         `date2`='$info[30]',`date3`='$info[31]',`date4`='$info[32]',`date5`='$info[33]',`date6`='$info[34]',`date7`='$info[35]',`date8`='$info[36]',`date9`='$info[37]',`date10`='$info[38]',`date11`='$info[39]',`date12`='$info[40]',
@@ -247,7 +247,7 @@ try{
 <body>
 
 <form method="POST" action="childDetails2.php">
-<input type="text" name="patient_id" placeholder="රෝගියාගේ හැඳුනුම්පත් අංකය සඳහන් කරන්න/Enter patient's NIC here" value="<?php echo($patient_id);?>"><br><br>
+<input type="text" name="id" placeholder="රෝගියාගේ හැඳුනුම්පත් අංකය සඳහන් කරන්න/Enter patient's NIC here" value="<?php echo($id);?>"><br><br>
 
 <input type="submit" name="Search" value="Search">
 <input type="submit" name="Update" value="Update"><br><br>
