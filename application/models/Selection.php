b041cdad66f3154ccd8017c12f8c09549170e91a
Jamini <jamini.18@cse.mrt.ac.lk>
2020-08-14 03:30:05 +0530

    pdf position corrected

--- application/models/Selection.php
+++ application/models/Selection.php
@@ -2,11 +2,11 @@
 Class Selection{
     function myFunction($id){
         if(strlen($id)==10) {
-            echo("<p style='font-size: 17px;border:1px solid black; position:absolute; height: 30px; width:25%;transform: translate(147%,500px); border-radius:5%; text-align: center;background-color:rgb(8, 184, 125);'>"."<a href='Client_pdf.php' style='color:black;'>Mother's Report</a>"."</p>");
+            echo("<p style='font-size: 17px;border:1px solid black; position:absolute; height: 30px; width:25%;transform: translate(147%,550px); border-radius:5%; text-align: center;background-color:rgb(8, 184, 125);'>"."<a href='Client_pdf.php' style='color:black;'>Mother's Report</a>"."</p>");
         
         }
         else{
-            echo("<p style='font-size: 17px;border:1px solid black; position:absolute; height: 30px; width:25%;transform: translate(147%,500px); border-radius:5%; text-align: center;background-color:rgb(8, 184, 125);'>"."<a href='Client.php' style='color:black;'>Child's Report</a>"."</p>");
+            echo("<p style='font-size: 17px;border:1px solid black; position:absolute; height: 30px; width:25%;transform: translate(147%,550px); border-radius:5%; text-align: center;background-color:rgb(8, 184, 125);'>"."<a href='Client.php' style='color:black;'>Child's Report</a>"."</p>");
         }}
 }
 
