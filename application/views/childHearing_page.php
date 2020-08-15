248349918fc4bc8b285cc3eb278234587b46ce57
Adeesha <63795833+anw98@users.noreply.github.com>
2020-08-15 11:59:42 +0530

    Update childHearing_page.php

--- application/views/childHearing_page.php
+++ application/views/childHearing_page.php
@@ -3,7 +3,23 @@ require '../models/childHearing_model.php';
 ?>
 
 <!DOCTYPE html>
-<head></head>
+<head>
+        <body>
+            <div class="hero-image">
+                <img src="../../public/images/logo.png" class="logo-image"/>
+                <img src="../../public/images/line.jpg" class="line-image"/>
+                <a href="login_page.php"><img src="../../public/images/logout.png" class="logout-image"></a>
+                <a href="home.php"><img src="../../public/images/home.png" class="home-image"></a>
+                <div class="hero-text">
+                    <h1 style="font-size:25px">Office of the Medical Officer of Health</h1>
+                    <h3 style="font-size:25px">Gampaha</h3>
+                </div>
+            </div>
+        </body>
+    </head>
+<head>
+	 <link rel ="stylesheet" type="text/css" href="../../public/css/newForm2.css">
+</head>
 <body>
 <form method="POST" action="childHearing_page.php">
 <input type="text" name="id" placeholder="රෝගියාගේ හැඳුනුම්පත් අංකය සඳහන් කරන්න/Enter patient's NIC here" value="<?php echo($id);?>"><br><br>
