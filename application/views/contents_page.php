0b25f51fc8b514ec70f730ae823fdf31888acc8a
Krishalika <krishalikadilanirathnayake@gmail.com>
2020-09-15 22:08:51 +0530

    corrections

--- application/views/contents_page.php
+++ application/views/contents_page.php
@@ -10,7 +10,7 @@
             <div class="hero-image">
                 <img src="../../public/images/logo.png" class="logo-image"/>
                 <img src="../../public/images/line.jpg" class="line-image"/>
-                <a href="login_page.php"><img src="../../public/images/logout.png" class="logout-image"></a>
+                <a href="login_test.php"><img src="../../public/images/logout.png" class="logout-image"></a>
                 <a href="home.php"><img src="../../public/images/home.png" class="home-image"></a>
                 <div class="hero-text">
                     <h1 style="font-size:25px">Office of the Medical Officer of Health</h1>
