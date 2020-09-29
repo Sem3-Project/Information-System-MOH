703f5681f35d76c17c56a7d8d5d79cf6e47118f9
Supuni-Jayasinghe <63720402+Supuni-Jayasinghe@users.noreply.github.com>
2020-09-29 12:35:08 +0530

    Update BMIcalc_page.php

--- application/views/BMIcalc_page.php
+++ application/views/BMIcalc_page.php
@@ -5,25 +5,27 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Calculate your BMI</title>
-  <link rel="stylesheet" href="../../public/css/bootstrap.min.css">
+  <link rel ="stylesheet" type="text/css" href="../../public/css/newForm.css">
+
+  <!--<link rel="stylesheet" href="../../public/css/bootstrap.min.css">-->
   <!-- <link rel = "stylesheet" href ="../../public/css/BasicDetails.css"/> -->
 
 </head>
 <!-- <body style="background-color:maroon;" > -->
-<body>
-<header>
-<div class="header"><img class="logo" src="../../public/images/logo.jpg"/>
-<div class="logo">
-		<h1>Medical Officer of Health Office</h1>
-		<h3>Gampaha</h3>
-    </div>
-
-<div class="topnav">
-  <a href="../controllers/logout.php">Log out</a>
-
-  
-</div>
-	</div>
+<head>
+        <body>
+            <div class="hero-image">
+                <img src="../../public/images/logo.png" class="logo-image"/>
+                <img src="../../public/images/line.jpg" class="line-image"/>
+                <a href="../controllers/logout.php"><img src="../../public/images/logout.png" class="logout-image"></a>
+                <a href="newHome.php"><img src="../../public/images/home.png" class="home-image"></a>
+                <div class="hero-text">
+                    <h1 style="font-size:25px">Office of the Medical Officer of Health</h1>
+                    <h3 style="font-size:25px">Gampaha</h3>
+                </div>
+            </div>
+        </body>
+    </head>
 <!-- </header>
   <header class="mt-3 text-center row">
   
@@ -39,35 +41,33 @@
     <header class="mt-3 text-center row">
   
   <div class="col-sm-10 align-right">
-<hr>
-    <h1 class="text-secondary">Calculate Body Mass Index</h1>
-  <hr>
-  </div>
+  <div class="caption-container1">
+                <div class="caption-container"><h2>Calculate Body Mass Index</h2></div>
+  
 
-</header>
+
       <div class="form-group row">
-        <label class="col-sm-2 col-form-label" for="weight" style="color:white">Weight in kg.</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" id="weight" name="weight" placeholder="Enter your weight in kilograms.">
-        </div>
+      <div class="caption-container"><h4>Weight in kg.</h4></div>
+          <input type="text" style="text-align:center; width: 50%; height: 50px; padding:10px; font-size:15px; " class="form-control" id="weight" name="weight" placeholder="Enter your weight in kilograms.">
+        
       </div>
       <div class="form-group row">
-        <label class="col-sm-2 col-form-label" for="height" style="color:white" >Height in cm.</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" id="height" name="height" placeholder="Enter your height in centimeters.">
-        </div>
+      <div class="caption-container"><h4>Height in cm.</h4></div>
+          <input type="text" style="text-align:center; width: 50%; height: 50px; padding:10px; font-size:15px; " class="form-control" id="height" name="height" placeholder="Enter your height in centimeters.">
+          
       </div>
+      <br><br>
       <div class="form-group mt-3 row">
         <div class="col-sm-2"></div>
         <div class="col-sm-10 align-right">
-          <button type="submit" class="btn btn-primary btn-block" name="calculate">Calculate</button>
+          <input type="submit"  class="link"  style="margin-bottom: 50px; " name="calculate" value="Calculate">
         </div>
       </div>
     </form>
     <div class="row">
       <div class="col-sm-2"></div>
-      <div class="col-sm-10 align-right">
-      
+      <div class="col-sm-10 align-right"></div>
+
       <?php
 
       
@@ -77,22 +77,20 @@
           $bmiRounded = round($bmi, 1);
           
             if($bmiRounded <= 18.4){
-              echo $BMIcolor = "#375FEF";
+              $output = "UNDERWEIGHT";
             }
-            if($bmiRounded >= 18.5 && $bmiRounded <= 24.9){
-              echo $BMIcolor = "#39C117";
+            else if($bmiRounded >= 18.5 && $bmiRounded <= 24.9){
+              $output = "NORMAL WEIGHT";
             }
-            if($bmiRounded >= 25 && $bmiRounded <= 29.9){
-              echo $BMIcolor = "#DFAE23";
+            else if($bmiRounded >= 25 && $bmiRounded <= 29.9){
+              $output = "OVERWEIGHT";
             }
-            if($bmiRounded >= 30 && $bmiRounded <= 39.9){
-              echo $BMIcolor = "#F68A33";
+            else if($bmiRounded >= 30 && $bmiRounded <= 39.9){
+              $output = "OBESE";
             }
-            else {
-              echo $BMIcolor = "#330906";
-            }
+            echo "<h2 >Your BMI value is  ${bmiRounded}   and you are : "; 
+echo "$output";
 
-          echo "<h2 style='color:" . $BMIcolor . "'>Your BMI is ${bmiRounded}</h2>";
         }
 
 
@@ -112,8 +110,10 @@
           calculateBMI($weight, $height);
         }
     ?>
+    <br><br>
     </div>
     </div>
   </div>
+  </div> 
 </body>
 </html>
