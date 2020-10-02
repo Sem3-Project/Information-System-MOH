e1337233732731364596050b4319134d670a31c4
Supuni-Jayasinghe <63720402+Supuni-Jayasinghe@users.noreply.github.com>
2020-10-02 21:55:50 +0530

    Update childDetails_model.php

--- application/models/childDetails_model.php
+++ application/models/childDetails_model.php
@@ -5,85 +5,84 @@ require '../../framework/libraries/Model.php';
 require '../models/table.php';
 
 $dbObj = Model::getInstance();
-$dbObj->connect('localhost', 'root', '', 'moh');
+$dbObj->connect('localhost', 'root', "", 'moh');
 session_start();
 
-$id='';
-$doctor='';
-$moh='';
-$idnum='';
-$childname='';
-$regno='';
-$childbday='';
-$regdate='';
-$momname='';
-$momage='';
-$num1='';
-$weight1='';
-$headperi='';
-$length='';
-$health='';
-$vitk='';
-$posture='';
-$rel='';
-$date1='';
-$date2='';
-$date3='';
-$date4='';
-$date5='';
-$date6='';
-$date7='';
-$date8='';
-$date9='';
-$date0='';
-$date10='';
+$id="";
+$doctor="";
+$moh="";
+$idnum="";
+$childname="";
+$regno="";
+$childbday="";
+$regdate="";
+$momname="";
+$momage="";
+$num1="";
+$weight1="";
+$headperi="";
+$length="";
+$health="";
+$vitk="";
+$posture="";
+$rel="";
+$date1="";
+$date2="";
+$date3="";
+$date4="";
+$date5="";
+$date6="";
+$date7="";
+$date8="";
+$date9="";
+$date0="";
+$date10="";
 
 //get data from the form
-function getData(){
-    $data=array();
-    $data[0]=(isset($_POST['id']) ? $_POST['id'] : '');
-    $data[1]=(isset($_POST['doctor']) ? $_POST['doctor'] : '');
-    $data[2]=(isset($_POST['moh']) ? $_POST['moh'] : '');  
-    $data[3]=(isset($_POST['idnum']) ? $_POST['idnum'] : '');
-    $data[4]=(isset($_POST['childname']) ? $_POST['childname'] : '');
-    $data[5]=(isset($_POST['regno']) ? $_POST['regno'] : '');
-    $data[6]=(isset($_POST['childbday']) ? $_POST['childbday'] : '');
-    $data[7]=(isset($_POST['regdate']) ? $_POST['regdate'] : '');
-    $data[8]=(isset($_POST['momname']) ? $_POST['momname'] : '');
-    $data[9]=(isset($_POST['momage']) ? $_POST['momage'] : '');
-    $data[10]=(isset($_POST['num1']) ? $_POST['num1'] : '');
-    $data[11]=(isset($_POST['weight1']) ? $_POST['weight1'] : '');
-    $data[12]=(isset($_POST['headperi']) ? $_POST['headperi'] : '');
-    $data[13]=(isset($_POST['length']) ? $_POST['length'] : '');
-    $data[14]=(isset($_POST['health']) ? $_POST['health'] : '');
-    $data[15]=(isset($_POST['vitk']) ? $_POST['vitk'] : '');
-    $data[16]=(isset($_POST['posture']) ? $_POST['posture'] : '');
-    $data[17]=(isset($_POST['rel']) ? $_POST['rel'] : '');
-    $data[18]=(isset($_POST['date1']) ? $_POST['date1'] : '');
-    $data[19]=(isset($_POST['date2']) ? $_POST['date2'] : '');
-    $data[20]=(isset($_POST['date3']) ? $_POST['date3'] : '');
-    $data[21]=(isset($_POST['date4']) ? $_POST['date4'] : '');
-    $data[22]=(isset($_POST['date5']) ? $_POST['date5'] : '');
-    $data[23]=(isset($_POST['date6']) ? $_POST['date6'] : '');
-    $data[24]=(isset($_POST['date7']) ? $_POST['date7'] : '');
-    $data[25]=(isset($_POST['date8']) ? $_POST['date8'] : '');
-    $data[26]=(isset($_POST['date9']) ? $_POST['date9'] : '');
-    $data[27]=(isset($_POST['date0']) ? $_POST['date0'] : '');
-    $data[28]=(isset($_POST['date10']) ? $_POST['date10'] : '');
-    return $data;
+function getPosts(){
+    $posts=array();
+    $posts[0]=$_POST['id'];
+    $posts[1]=$_POST['doctor'];
+    $posts[2]=$_POST['moh'];  
+    $posts[3]=$_POST['idnum'];
+    $posts[4]=$_POST['childname'];
+    $posts[5]=$_POST['regno'];
+    $posts[6]=$_POST['childbday'];
+    $posts[7]=$_POST['regdate'];
+    $posts[8]=$_POST['momname'];
+    $posts[9]=$_POST['momage'];
+    $posts[10]=$_POST['num1'];
+    $posts[11]=$_POST['weight1'];
+    $posts[12]=$_POST['headperi'];
+    $posts[13]=$_POST['length'];
+    $posts[14]=$_POST['health'];
+    $posts[15]=$_POST['vitk'];
+    $posts[16]=$_POST['posture'];
+    $posts[17]=$_POST['rel'];
+    $posts[18]=$_POST['date1'];
+    $posts[19]=$_POST['date2'];
+    $posts[20]=$_POST['date3'];
+    $posts[21]=$_POST['date4'];
+    $posts[22]=$_POST['date5'];
+    $posts[23]=$_POST['date6'];
+    $posts[24]=$_POST['date7'];
+    $posts[25]=$_POST['date8'];
+    $posts[26]=$_POST['date9'];
+    $posts[27]=$_POST['date0'];
+    $posts[28]=$_POST['date10'];
+    return $posts;
   }
 
 $childDetails = new table();
 
 //search
-if(isset($_POST['Search'])){
-    $info=getData();
-    $search_query="SELECT * FROM childdata1 WHERE id='$info[0]'";
-    $search_result=$childDetails->featuredLoad($dbObj,$search_query);
-        if($search_result){
-            if($search_result){
-                if(mysqli_num_rows($search_result)){
-                    while($row = mysqli_fetch_array($search_result)){
+if(isset($_POST['search'])){
+    $data=getPosts();
+    $search_Query="SELECT * FROM childdata1 WHERE id='$data[0]'";
+    $search_Result=$childDetails->featuredLoad($dbObj,$search_Query);
+        if($search_Result){
+                if(mysqli_num_rows($search_Result)){
+                    while($row = mysqli_fetch_array($search_Result)){
                         $id=$row['id'];
                         $doctor=$row['doctor'];
                         $moh=$row['moh'];
@@ -125,17 +124,21 @@ if(isset($_POST['Search'])){
               echo '}, 200);</script>';
             }
         }
-    }
+    
   
   //update
-  if(isset($_POST['Update'])){
-      $info=getData();
-      $update_query="UPDATE childdata1 SET id='$info[0]',doctor='$info[1]',moh= '$info[2]',idnum='$info[3]',childname='$info[4]',regno='$info[5]',childbday='$info[6]',regdate='$info[7]',momname='$info[8]',momage='$info[9]',num1='$info[10]',weight1='$info[11]',headperi='$info[12]',length='$info[13]',health='$info[14]',vitk='$info[15]'
-      ,posture='$info[16]',rel='$info[17]',date1= '$info[18]',date2='$info[19]',date3='$info[20]',date4='$info[21]',date5='$info[22]',date6='$info[23]',date7='$info[24]',date8= '$info[25]',date9='$info[26]',date0='$info[27]',date110='$info[28]'  WHERE id='$info[0]'";
+  if(isset($_POST['update'])){
+      $data=getPosts();
+      $update_Query="UPDATE childdata1 SET id='$data[0]',doctor='$data[1]',moh= '$data[2]',idnum='$data[3]',
+      childname='$data[4]' , regno='$data[5]',childbday='$data[6]',regdate='$data[7]',momname='$data[8]',
+      momage='$data[9]',num1='$data[10]',weight1='$data[11]',headperi='$data[12]',length='$data[13]',
+      health='$data[14]',vitk='$data[15]',posture='$data[16]',rel='$data[17]',date1= '$data[18]',
+      date2='$data[19]',date3='$data[20]',date4='$data[21]',date5='$data[22]',date6='$data[23]',date7='$data[24]',
+      date8= '$data[25]',date9='$data[26]',date0='$data[27]',date10='$data[28]'  WHERE id='$data[0]'";
 
 try{
-  $pdate_result=$childDetails->featuredLoad($dbObj,$update_query);
-  if($pdate_result){
+  $update_Result=$childDetails->featuredLoad($dbObj,$update_Query);
+  if($update_Result){
     echo '<script type="text/javascript">';
             echo 'setTimeout(function () { swal("Success!","Data updated successfully!","success");';
             echo '}, 200);</script>';
@@ -147,3 +150,4 @@ try{
 }
 }
 ?>
+
