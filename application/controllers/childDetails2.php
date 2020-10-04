e29622670a122996b0a0c83cd796f6bf6eb36765
Adeesha <63795833+anw98@users.noreply.github.com>
2020-10-04 10:58:31 +0530

    Add files via upload

---
+++ application/controllers/childDetails2.php
@@ -0,0 +1,50 @@
+<?php
+
+require '../../framework/libraries/Model.php';
+
+class ChildDetail2 extends Child{
+  
+
+    var $id = null;
+    var $dat1 = null;
+    var $dat2 =null;  
+    var $dat3 = null;
+    var $dat4 = null; 
+    var $tableName = "childdata2";
+
+    var $scolor1 = null;
+    var $scolor2 = null;
+    var $scolor3 = null;
+    var $scolor4 = null;
+
+    var $eye1 = null;     var $eye2 = null;
+    var $eye3 = null;     var $eye4 = null;
+
+    var $sc1 = null;
+    var $sc2 = null;
+    var $sc3 = null;
+    var $sc4 = null;
+
+    var $m1 = null;     var  $m2 = null;      
+    var  $m3 = null;    var $m4 = null;       
+    var  $pos1 = null;      var $pos2 = null;     
+    var  $pos3 = null;      var  $pos4 = null;
+    var $rel1 = null;    var $rel2 = null;    
+    var $rel3 = null;    var $rel4 = null;   
+
+    var $other1 = null;       var $other2 = null;
+    var $other3 = null;       var $other4 = null;
+    var $date1 = null;       var $date2 = null;
+    var $date3 = null;       var $date4 = null;
+    var $tdate5 = null;       var $date6 = null;
+    var $date7 = null;          var $date8 = null;
+    var $date9 = null;       var $date10 = null;
+    var $date11 = null;       var $date12 = null;
+    var $tdate13 = null;       var $date14 = null;
+    var $date15 = null;          var $date16 = null;
+    var $date17 = null;       var $date18 = null;
+    var $date19 = null;       var $date20 = null;
+   
+
+}
+?>
