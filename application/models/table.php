84a90cf0ba61fc42118f063db65d7ad129eaee22
Krishalika <krishalikadilanirathnayake@gmail.com>
2020-12-15 23:46:36 +0530

    changes in table class

--- application/models/table.php
+++ application/models/table.php
@@ -17,33 +17,7 @@ class table
             $sql = "SELECT * from {$this->tableName} where id = '{$this->id}'";
             return $sql;
         } 
-        // elseif ($task == 'insert') {
-        //     $keys = "";
-        //     $values = "";
-        //     $classAttributes = get_class_vars(get_class($this));
-        //     $sql .= "INSERT into {$this->tableName} ";
-        //     foreach ($classAttributes as $key => $value) {
-        //         if ($key == "tableName") {
-        //             continue;
-        //         }
-        //         $keys .= "{$key},";
-        //         $values .= "'{$this->$key}',";
-        //     }
-        //     $sql .= "(" . substr($keys, 0, -1) . ") Values(" . substr($values, 0, -1) . ")";
-        //     return $sql;
-        // } elseif ($task == 'update') {
-        //     $classAttributes = get_class_vars(get_class($this));
-        //     $sql .= "UPDATE {$this->tableName} set ";
-        //     foreach ($classAttributes as $key => $value) {
-        //         if ($key == "id" || $key == "tableName") {
-        //             continue;
-        //         }
-        //         $sql .= "{$key} = '{$this->$key}',";
-        //     }
-        //     $sql = substr($sql, 0, -1) . " where id = '{$this->id}'";
-        //     return $sql;
-
-        // }
+     
     }
 
     function load($dbObj, $id)
@@ -69,28 +43,10 @@ class table
 
     }
 
-    // function insert($dbObj)
-    // {
-    //     $sql = $this->buildQuery('insert');
-    //     $dbObj->doQuery($sql);
-    // }
-
-    // function update($dbObj)
-    // {
-    //     $sql = $this->buildQuery('update');
-    //     $dbObj->doQuery($sql);
-    // }
-
     function featuredLoad($dbObj,$sql){
         $result = mysqli_query($dbObj->getConnection(),$sql) or die("Database access failed..!!");
         return $result;
     }
 
-    // function bind($data)
-    // {
-    //     foreach ($data as $key => $value) {
-    //         $this->$key = $value;
-    //     }
-    // }
 }
 ?>
